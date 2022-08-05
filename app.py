f66e9edc115b69fe7013478637ff4d9d01d7d580
mm74noroozi <41954275+mm74noroozi@users.noreply.github.com>
2022-08-06 01:02:28 +0430

    asyncio

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from asyncio.log import logger
 from datetime import datetime
+import asyncio
 from yaml import safe_load as load
 from pprint import pprint
 from paramiko import SSHClient,AutoAddPolicy
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s: %(message)s',level=logging.INFO)
 
 with open("config.yml","r+") as f:
     obj=load(f.read())
@@ -9,19 +15,21 @@ with open("config.yml","r+") as f:
     commands=obj["commands"]
 
 
-pprint(vms)
 
-sessions=[]
-
-for item in vms:
+async def run_commands(vm,commands):
+    logger=logging.getLogger(vm["name"])
     with SSHClient() as client:
         client.load_system_host_keys()
         client.set_missing_host_key_policy(AutoAddPolicy())
-        client.connect(item["host"], username='root',port=2223,look_for_keys=True)
-        print(f'\n\n----------------------{item["name"]}--------------------------')
+        client.connect(vm["host"], username='root',port=2223,look_for_keys=True)
         for command in commands:
-            print("->",command)
-            stdin,stdout,stderr = client.exec_command(command)
-            print(stdout.read().decode("utf8"))
-    print(f"------------{datetime.now()}-----------------")
+            logger.info("->"+command)
+            _,stdout,__ = client.exec_command(command)
+            logger.info(stdout.read().decode("utf8"))
 
+async def main():
+    jobs=[run_commands(vm,commands) for vm in vms]
+    await asyncio.gather(*jobs)
+
+if __name__ == "__main__":
+    asyncio.run(main())
